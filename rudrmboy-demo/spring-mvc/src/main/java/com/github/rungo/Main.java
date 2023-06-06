856fcea5c90958a3f082384901a1ee3486748e4d
Rungo <rungo8803@163.com>
2023-06-06 13:52:01 +0800

    mvc demo -  demo

--- rudrmboy-demo/spring-mvc/src/main/java/com/github/rungo/Main.java
+++ rudrmboy-demo/spring-mvc/src/main/java/com/github/rungo/Main.java
@@ -1,15 +1,20 @@
 package com.github.rungo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.rungo.rudrmboy.demo.spring.mvc.config.exception.IONoMoneyException;
 import com.github.rungo.rudrmboy.demo.spring.mvc.convert.MyModel;
+import com.github.rungo.rudrmboy.demo.spring.mvc.pojo.Child;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IONoMoneyException {
         System.out.println("Hello world!");
 
 
@@ -31,8 +36,10 @@ public class Main {
     }
 
     @Test
-    public void test1(){
+    public void test1() throws IONoMoneyException {
 
+
+        throw new IONoMoneyException("KFC Crazy Thursday need 50RMB");
     }
 
     public class MyClassLoader extends ClassLoader {
@@ -46,6 +53,46 @@ public class Main {
             }
             return super.loadClass(name, resolve);
         }
+    }
+
+
+    @Test
+    public void firstSpringSource() {//初始化容器
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-config-${dev}.xml");
+    }
+
+
+    @Test
+    public void test2(){
+        //系统环境属性
+        Map<String, String> map = System.getenv();
+        System.out.println(map);
+        //JVM系统属性
+        Properties properties = System.getProperties();
+        System.out.println(properties);
+    }
+
+
+    @Test
+    public void test3(){
+        Child child = new Child();
+    }
+
+
+    @Test
+    public void setProp() {
+        System.setProperty("x","config");
+        //初始化容器
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-${x}.xml");
+    }
+
+
+    @Test
+    public void circularPlaceholderReference() {
+        //config key 对的value为${config}，在解析value时又会找到config key，造成循环变量引用…………
+        System.setProperty("config", "${config}");
+        //初始化容器
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-${config}.xml");
     }
 
 }
