240105b021a1a23af2b5808fd9c77755326941e0
Rungo <rungo8803@163.com>
2023-06-25 17:03:07 +0800

    core demo -  源码分析

--- rudrmboy-demo/spring-core/src/test/java/com/github/rungo/test/initPropertySourcesTest.java
+++ rudrmboy-demo/spring-core/src/test/java/com/github/rungo/test/initPropertySourcesTest.java
@@ -1,7 +1,10 @@
 package com.github.rungo.test;
 
 import com.github.rungo.rudrmboy.demo.spring.context.support.RequiredPropertieClassPathXmlApplicationContext;
+import com.github.rungo.rudrmboy.demo.spring.source.BeanPostProcessorTest;
+import com.github.rungo.rudrmboy.demo.spring.source.ingoreInterface.IgnoreOther;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 /**
@@ -53,5 +56,50 @@ public class initPropertySourcesTest {
         System.out.println(ac.getBean("indexClass2"));
         System.out.println(ac.getBean("indexClass3"));
         System.out.println(ac.getBean("indexClass4"));
+    }
+
+
+    @Test
+    public void beanPostProcessorTest() {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-config-test.xml");
+        //手动注入，不会触发BeanPostProcessor回调（这里使用了 registerSingleton(...) 方法来注册单例 Bean，即在整个应用程序上下文中，只创建一个该类型的 Bean 实例。）
+        ac.getBeanFactory().registerSingleton("beanPostProcessorTest2", new BeanPostProcessorTest("registerSingleton"));
+    }
+
+
+    @Test
+    public void ingoreInterfaceTest() {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-config-test.xml");
+        //手动注入，不会触发BeanPostProcessor回调（这里使用了 registerSingleton(...) 方法来注册单例 Bean，即在整个应用程序上下文中，只创建一个该类型的 Bean 实例。）
+        ac.getBeanFactory().ignoreDependencyInterface(Ignore.class);
+    }
+
+
+    @Test
+    public void ignoreDependencyInterface() {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-config-test.xml");
+        System.out.println(ac.getBean("ignoreByName"));
+        System.out.println(ac.getBean("ignoreByType"));
+        System.out.println(ac.getBean("ignoreByConstructor"));
+        System.out.println("---------------------------------------");
+        System.out.println(ac.getBean("IgnoreOtherByType"));
+        System.out.println(ac.getBean("IgnoreOtherByName"));
+        System.out.println(ac.getBean("IgnoreOtherByConstructor"));
+    }
+
+    @Test
+    public void registerResolvable() {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-config-test.xml");
+        System.out.println(ac.getBean("autoRegister"));
+    }
+
+
+    @Test
+    public void myRegisterResolvable() {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring-config-test.xml");
+
+        System.out.println(ac.getBean("rd"));
+        System.out.println(ac.getBean("rdImplA"));
+        System.out.println(ac.getBean("rdImplB"));
     }
 }
