f3a9af636da3ebca3032e85c78698ca0b0c53fe5
Rungo <rungo8803@163.com>
2023-12-23 15:08:46 +0800

    调整目录结构

---
+++ letool-starter-tool/src/main/java/com/github/leyland/letool/tool/util/JsonUtil.java
@@ -0,0 +1,13 @@
+package com.github.leyland.letool.tool.util;
+
+/**
+ * 对 FastJson 进行二次封装的工具类
+ *
+ * <h2>JsonUtil</h2>
+ *
+ * @author: leyland
+ * @date: 2023.12
+ * @version: 1.0
+ **/
+public class JsonUtil {
+}
