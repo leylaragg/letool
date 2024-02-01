52366546d9334c7f66c5d3a12ca26fdd6033753d
leyland <leyland.wang@aliyun.com>
2024-02-01 19:08:22 +0800

    新增 ClassUtil 工具类，暂未完善，需继续补充

---
+++ letool-starter-tool/src/main/java/com/github/leyland/letool/tool/util/ClassUtil.java
@@ -0,0 +1,13 @@
+package com.github.leyland.letool.tool.util;
+
+import org.springframework.util.ClassUtils;
+
+/**
+ * @ClassName <h2>ClassUtil</h2>
+ * @Description TODO Class 工具类
+ * @Author Rungo
+ * @Version 1.0
+ **/
+public class ClassUtil {
+
+}
